bc72a9edd7d184419d41d19ac9e39fd091b0277a
Jordi Belmonte <jorte.03@gmail.com>
2021-02-14 13:13:06 -0300

    Styles and Header changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,9 +3,14 @@ import {Link} from 'react-router-dom'
 const Header = () =>{
     return (
         <>
-        <h1>Holaaaaaaaa</h1>
-        <Link to ='/login'><p>LogIn</p></Link>
-        <Link to ='/signup'><p>SignUp</p></Link>
+        <div id="headerContainer" style={{backgroundColor:"#11050F"}}>
+            <div className="logo" style={{backgroundImage: `url("../assets/logo")`}}></div>
+            <div className="links">
+                <Link to ='/login'><p>LogIn</p></Link>
+                <Link to ='/signup'><p>SignUp</p></Link>
+            </div>
+        </div>
+
         </>
         
     )
