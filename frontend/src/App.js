4340c24a940acd91eee5b9053fb801112e28094f
Joaquin <joa12386@gmail.com>
2021-02-14 11:11:13 -0300

    Conexion de css a index

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import {Route,BrowserRouter,Switch} from 'react-router-dom'
 import LogIn from './pages/Login';
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import LatestNews from './components/LatetsNews';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path='/' component={Header}/>
         <Route path='/signup' component={SignUp}/>
         <Route path='/login' component={LogIn}/>
+        <Route path='/test' component={LatestNews}/>
         </Switch>
       </BrowserRouter>
       
---
+++ frontend/src/components/LatetsNews.js
@@ -0,0 +1,12 @@
+
+const LatestNews = () =>{ 
+    return (
+        <div>
+            <div >
+                <h2 className="textCenter">Latest News</h2>
+            </div>
+        </div>
+    )
+}
+
+export default LatestNews
