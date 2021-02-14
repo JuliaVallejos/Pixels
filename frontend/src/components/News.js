2cc1acd91f6ab2a858915007c70c4004e2d79102
Joaquin <joa12386@gmail.com>
2021-02-14 15:01:17 -0300

    card casi terminada

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -1,10 +1,12 @@
 const News = () =>{ 
     return (
         <div id="cardPadre" className="justifyCenter ">  
-            <div className="bordes estiloCardIt estiloCard cardHijo" id="bordes" style={{ backgroundImage: `url("https://images3.alphacoders.com/863/863669.jpg")` }}>
+            <div className="bordes estiloCardIt estiloCard cardHijo justifyFlexEnd" style={{ backgroundImage: `url("https://images3.alphacoders.com/863/863669.jpg")` }}>
                 <div className="news ">
-                    <h3>READY OR NOT, HERE I COME!</h3>
-                    <h2>I HAVE ARRIVED ON STEAM AND VENGEANCE WILL THE MINE!</h2>
+                    <div className="texto">
+                        <h3>READY OR NOT, HERE I COME!</h3>
+                        <h2>I HAVE ARRIVED ON STEAM AND VENGEANCE WILL THE MINE!</h2>
+                    </div>
                 </div>
             </div>
         </div>   
