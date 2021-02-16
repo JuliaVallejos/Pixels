c09da4c0dd73236800369900f673b3c1d78733df
Fran <franciscoaflorentin@hotmail.com>
2021-02-16 09:33:33 -0300

    conflicts

--- frontend/src/redux/reducers/rootReducer.js
+++ frontend/src/redux/reducers/rootReducer.js
@@ -2,13 +2,9 @@ import { combineReducers } from "redux"
 import gamesReducer from "./gamesReducer"
 import usersReducer from './usersReducer'
 const rootReducer = combineReducers({
-<<<<<<< HEAD
-    user: usersReducer 
-=======
     
     user: usersReducer,
     game:gamesReducer
->>>>>>> 9e796a7fd41fc74714ef2290da3aa8cfa4272643
  
 })
 
