472bf315cb02a334e9abfa43f533ccbc147691cb
Fran <franciscoaflorentin@hotmail.com>
2021-02-16 09:25:16 -0300

    asd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import LogIn from './pages/Login';
 import SignUp from './pages/SignUp'
-import Header from './components/Header'
 import Category from './components/Category';
-import Footer from './components/Footer'
 import usersActions from "./redux/actions/usersActions"
 import {connect} from "react-redux"
 
@@ -21,12 +19,8 @@ function App({loggedUser,login_with_LS}) {
           <Route path='/test' component={Category}/>
         </Switch>
         <Footer/>
-<<<<<<< HEAD
-      </BrowserRouter>    
-=======
       </BrowserRouter >
       
->>>>>>> 7cdd8a0cc67f5813b2a6069be0e48519e80843b5
     </div>
   )
 }
