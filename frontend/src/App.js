f3028d5b4c7e0d2fb9361bd8147923a7580fa647
Aman <amancayic@gmail.com>
2021-02-15 22:20:02 -0300

    Impor redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import {Route,BrowserRouter,Switch} from 'react-router-dom'
+import {Route,BrowserRouter,Switch,Redirect} from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import LogIn from './pages/Login';
