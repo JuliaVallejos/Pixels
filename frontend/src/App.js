156764eaee07ae091bf99e44a16130eed023721d
Aman <amancayic@gmail.com>
2021-02-17 16:22:05 -0300

    textarea

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import {Route,BrowserRouter,Switch,Redirect} from 'react-router-dom'
+import {connect} from "react-redux"
 import Header from './components/Header'
 import Library from './pages/Library'
 import Footer from './components/Footer'
@@ -8,8 +9,7 @@ import CategoryList from './pages/CategoryList'
 import Home from './pages/Home'
 import DeveloperPage from './pages/DeveloperPage'
 import usersActions from "./redux/actions/usersActions"
-import {connect} from "react-redux"
-import AddNews from './pages/AddNews'
+
 
 
 
@@ -27,14 +27,13 @@ function App({loggedUser,login_with_LS}) {
         {(loggedUser && loggedUser.userRol==="Developer")
         && <Route exact path='/developers' component={DeveloperPage}/>
         }
-        <Route path='/library' component={Library}/> 
-        <Route path='/news' component={AddNews}/>
-          {!loggedUser && 
+        {!loggedUser && 
           <>
             <Route path='/signup' component={SignUp}/>
             <Route path='/login' component={LogIn}/>
           </>
         }        
+        <Route path='/library' component={Library}/> 
         <Redirect to="/" />
         </Switch>
         <Footer/>
--- frontend/src/pages/DeveloperPage.js
+++ frontend/src/pages/DeveloperPage.js
@@ -1,114 +1,35 @@
-import {connect} from 'react-redux'
 import {useState,useEffect} from 'react'
-import gamesActions from '../redux/actions/gamesActions'
-
+import AddNews from '../components/AddNews'
+import AddGames from '../components/AddGames'
 
 
 const DeveloperPage = (props) =>{
-    const [errors,setErrors] = useState([])
-    const [newGame,setNewGame] = useState({
-        gameTitle:'',
-        gameInfo:'',
-        gameCategories:'',
-        clasificationPEGI:'',
-        gameImg:''
-    })
+    const [section, setSection] = useState(true)
 
-    console.log(props)
+    const changeSection = () => setSection (!section)
 
-    const read_input = e =>{
-        const property= e.target.name
-        const value = e.target.value
-
-        setNewGame({
-            ...newGame, 
-            [property]:value,
-            idUser: props.loggedUser.response.id
-        })      
-    }
-
-        // console.log(props.loggedUser.response.id)
-
-    const send_data= async e =>{
-        setErrors([])
-        e.preventDefault()
-
-        const {gameTitle,gameInfo,gameCategories,clasificationPEGI,gameImg} = newGame
-        
-        if(gameTitle==='' || gameInfo===''|| gameCategories ==='' || clasificationPEGI==='' || gameImg===''){
-           
-            setErrors([{message:'All required(*) fields must be completed'}])
-            return false        
-        }
-     
-        const data = await props.submitNewGame(newGame)
-
-        if(data && !data.sucess){
-            setErrors([data.errors])
-            alert('Error recording a new game')
-            console.log(errors)
-        }else {
-            alert('New game saved successfully')
-            //CLEAN INPUT FUNCTION
-        } 
-    }
-// CLEAN INPUTS
-
-    const clasificationPEGI = [3,7,12,16,18]
-    console.log(errors)
-    console.log(newGame)
-    
     return(
-        <div className="signUp centerCenter" style={{backgroundImage: `url("../assets/bricks.jpg")`, height: "65vh"}}>
-            <h2>Upload your game</h2>
-                <form>
-                    <input id='gameTitle' name='gameTitle' type='text' placeholder='Game Title*' onChange={read_input}/>
+        <> 
+        <div className="developerContainer signUp centerCenter" style={{backgroundImage: `url("../assets/bricks.jpg")`, height: "65vh"}}>
 
-                    <textarea id='gameInfo' name='gameInfo' type='text' placeholder='Game description*' onChange={read_input}/>
+            <div id="headerContainer" className="justifyAround ">
+                <div className="links justifyAround ">
+                    <p onClick={changeSection}>ADD GAME</p>
+                    <p onClick={changeSection}>ADD NEWS</p>
+                </div>
+            </div>
 
-                    <select name="gameCategories"onChange={read_input}>
-
-                        <option value="" disabled="true" selected="true">Select Category</option>
-
-                        {props.categories.map(category=>{
-                            return(<option value={category.name}>{category.name}</option>)
-                        })}
-                    </select>
-                    <select name="clasificationPEGI"onChange={read_input}>
-                        <option value="value1" disabled="true" selected="true">ClasificationPGI</option>
-                        {clasificationPEGI.map((clasification,index) =>{
-                            return(<option value={clasification} key={index}>{clasification}</option>)
-                        })}
-                    </select>
-
-                    <input type="text" name="gameImg" placeholder="Pic*" onChange={read_input}/>
-
-                    <button onClick={send_data} type='submit'>Submit</button>
-                    
-                    {/* {errors.length !==0 ?
-                        errors.map((error,index) =>{
-                            return (<p key={index}>{error.message}</p>)
-                        })
-                    : 
-                    return false
-
-                    } */}
-                </form>
+            <div className="developerSection">
+            {!section ? 
+            <AddNews/> : 
+            <AddGames/>
+            }
+            </div>
+    
         </div>
+        </>
     )
 }
 
-const mapStateToProps= state =>{
-    return {
-        gamesList: state.game.gamesList,
-        categories: state.game.categories,
-        loggedUser: state.user.loggedUser
-    }
-}
 
-const mapDispatchToProps= {   
-    submitNewGame: gamesActions.submitNewGame,
-    allGames: gamesActions.allGames
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(DeveloperPage)
+export default DeveloperPage
