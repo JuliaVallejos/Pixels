72e06c946f4e80553c859887f6f84c991816f81c
Julia Vallejos <jval.benes@gmail.com>
2021-02-18 18:25:18 -0300

    Arregle filtro

--- frontend/src/pages/Library.js
+++ frontend/src/pages/Library.js
@@ -8,6 +8,7 @@ import gamesActions from '../redux/actions/gamesActions'
 const Library = (props) =>{
     const {newGamesList} = props
     const [loading,setLoading] = useState(true)
+    const [editFilter,setEditFilter] = useState(false)
     /* const [newOrder,setNewOrder] =  useState(newGamesList) */
     
     const [agesState,setAgesState] = useState([])
@@ -24,6 +25,7 @@ const Library = (props) =>{
 
   
     const read_input= e =>{
+      /*   setEditFilter(true) */
         const search = e.target.value
         props.filterGames(search)
       
@@ -34,7 +36,8 @@ const Library = (props) =>{
         const data = await props.allGames()
         data&& setLoading(false)
     }
-    const filterPEGI = e =>{
+    const selectAges = e =>{
+        
         ages=ages.concat(agesState)
         const value=parseInt(e.target.value)
 
@@ -46,11 +49,15 @@ const Library = (props) =>{
            const ind= ages.indexOf(value)
             ages.splice(ind,1)
         }
-  
+        
     }
     const filt_games = () =>{
-        setGamesFilteredPEGI([])
-     
+        if(ages.length===0){
+            ages=ages.concat(agesState)
+        }
+        gamesConcat=[]
+
+     console.log(ages)
         ages.map(age=>{
            
              gamesFiltered= newGamesList.filter(game=> game.clasificationPEGI===age) 
@@ -58,7 +65,24 @@ const Library = (props) =>{
             })
      setGamesFilteredPEGI(gamesConcat)
      setAgesState(ages)
-    
+  
+  
+   
+    }
+    const clearAges = () =>{
+        setGamesFilteredPEGI([])
+        setAgesState([])
+        
+    }
+    const finish= () =>{
+        console.log(editFilter)
+   /*    if(!editFilter){
+          console.log('voy a borrar')
+            clearAges()
+      } */
+            
+        
+            filt_games()
         
     }
     
@@ -75,7 +99,7 @@ const Library = (props) =>{
         <h2  className="textCenter">Library</h2>
         <Categories/>
         <input type='text' onChange={read_input} placeholder='Search'/>
-        <label onChange={filterPEGI} htmlFor='PEGI'>Select clasification PEGI
+        <label onChange={selectAges} htmlFor='PEGI'>Select clasification PEGI
             <input type='checkbox' name='PEGI' value='3' />3
             <input type='checkbox' name='PEGI' value='7' />7
             <input type='checkbox' name='PEGI' value='12' />12
