e9a1049dd241f5fea861455ea58890355054642b
Joaquin <joa12386@gmail.com>
2021-02-20 16:57:10 -0300

    Redes sociales footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,7 @@
 import {NavLink} from 'react-router-dom'
 import {connect} from "react-redux"
 import usersActions from "../redux/actions/usersActions"
+import { TiSocialInstagram, TiSocialLinkedin, TiSocialFacebook, TiSocialTwitter } from 'react-icons/ti';
 
 const Footer = ({loggedUser,logOut}) => {
     return(
@@ -10,7 +11,17 @@ const Footer = ({loggedUser,logOut}) => {
                     <NavLink exact to='/'><p>Home</p></NavLink>
                     <NavLink to='/library'><p>Library</p></NavLink>
                     <NavLink to='/news'><p>News</p></NavLink>
+                    
+                    <div>
                     <NavLink to='/Contact'><p>Contact</p></NavLink>
+                        <div className="redesSociales">
+                            <TiSocialInstagram/>
+                            <TiSocialLinkedin/>
+                            <TiSocialFacebook/>
+                            <TiSocialTwitter/>
+                        </div>
+                    </div>
+                    
                     {(loggedUser && loggedUser.userRol==="Developer")
                     ? <NavLink to='/developers'><p>Developers</p></NavLink>
                     : <NavLink onClick={()=>alert("You need to be a developer")} exact to='#'><p>Developers</p></NavLink>
