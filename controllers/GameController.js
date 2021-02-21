976e920f80ec0fecb491c280bcbcbeb136046641
Fran <franciscoaflorentin@hotmail.com>
2021-02-21 00:28:01 -0300

    merge

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -1,12 +1,13 @@
+const { populate } = require('../models/Game');
 const Game = require('../models/Game')
 
 const GameController ={
     addGame: async (req, res) =>{
         var prom = 0
         const {gameTitle, gameCategories, gameInfo, valoration, userComments, clasificationPEGI, idUser}=req.body
-
+      
         const {gameFile} = req.files;
-
+        
         const imgType = gameFile.name.split(".").slice(-1).join(" ");
 
         const createGame = new Game({
@@ -18,7 +19,7 @@ const GameController ={
 
         await gameFile.mv(imgPath,error=>{
             if(error){
-                console.log("Todo mal")
+               
                 errors.push(error)}
             else{console.log("Todo OK")}
         })
@@ -27,11 +28,9 @@ const GameController ={
 
         createGame.save()
          
-
         .then( async savedGame =>{
            const game = await savedGame.populate('idUser').execPopulate() 
-           console.log("entró game") 
-           console.log(game)         
+             
             return res.json({success:true, response: game})
         })
         .catch(error=>{
@@ -41,7 +40,7 @@ const GameController ={
 
     allGames: (req, res)=>{
       
-        Game.find()
+        Game.find().populate('idGame').populate('userComments.idUser')
         .then(respuesta =>{
             return res.json({success: true, response: respuesta})
         })
@@ -57,14 +56,17 @@ const GameController ={
             return res.json({success: true, response: respuesta,message:'Game deleted'})
         })
         .catch(error =>{
+    
             return res.json({success: false, error: error})
         })
     
     },
-    
     gameById:(req, res)=>{
+ 
        const  id=req.params.idGame
-        Game.find({'_id':id}).populate('_id')
+
+        Game.findOne({'_id':id}).populate('idGame').populate('userComments.idUser')
+
         .then(respuesta=>{
             return res.json({success:true, response:respuesta})
         })
@@ -72,49 +74,88 @@ const GameController ={
             return res.json({success:false, response:error})
         })
     },
+
     addCommentsGames: (req, res)=>{
-           const {idUser, comment}=req.body
-           Game.findOneAndUpdate({_id:req.body.id}, {
+           const idUser = req.user._id
+           const {comment}=req.body
+           const id=req.params.idGame
+
+           
+          
+           Game.findOneAndUpdate({_id:id}, {
             $push:{
-             userComments:{idUser:idUser, comment:comment}
+             "userComments":{idUser:idUser, comment:comment}
             }
-        })
+            
+        },
+        {new: true}
+        ).populate('userComments.idUser')
+
         .then(respuesta =>{
+            
             return res.json({success:true, response:respuesta})
+        
+
         })
         .catch(error=>{
+     
             return res.json({success:false, response:error})
         })  
     },
     
-    modifyComment: (req, res)=>{
-        const {idUser, comment}=req.body
-        Game.findByIdAndUpdate({_id:req.body.id}, {
-         $set:{
-          userComments:{idUser:idUser, comment:comment}
-         }
-     })
-     .then(respuesta =>{
-         return res.json({success:true, response:respuesta})
-     })
-     .catch(error=>{
-         return res.json({success:false, response:error})
-     })  
+    editComment: (req, res)=>{
+       
+        const idGame= req.params.idGame
+        const idComment=req.params.idComment
+        const editedComment = req.body.editedComment
+ 
+       Game.findOneAndUpdate({_id:idGame,'userComments._id':idComment},{ $set: {'userComments.$.comment':editedComment}},{new:true}).populate('idUser').populate('userComments.idUser')
+        .then( response => {
+
+            res.json({success:true,message:'Comment edited',response})})
+        .catch(error => res.json({success:false,error}))
     }, 
+    
     deleteComment: (req, res)=>{
-        const idGames= req.params.idgame
-        const idComment = req.params.idcomment
+
+        const idGames= req.params.idGame
+        const idComment = req.params.idComment
+
         Game.findByIdAndUpdate({_id: idGames},{
             $pull:{
                 userComments:{_id:idComment}
             }
-        })
+        },{new:true}).populate('userComments.idUser')
         .then(respuesta=>{
             return res.json({success:true, response:respuesta, message:"delete comment"})
         })
         .catch(error=>{
             return res.json({success:false, response:error})
         })
+
+    },
+    setValoration: (req,res) =>{
+        const idGame= req.params.idGame
+        const {idUser,valoration} = req.body
+        const newVal ={idUser,valoration}
+      
+        if(req.body.edit){
+            Game.findOneAndUpdate({_id:idGame,'valoration.idUser':idUser},{ $set: {'valoration.$.valoration':valoration}},{new:true}).populate('userComments.idUser')
+             .then(respuesta =>{
+                return res.json({success:true, response:respuesta})
+            })
+            .catch(error=>{
+                return res.json({success:false, response:error})
+            })
+            }else{
+        Game.findOneAndUpdate({_id:idGame}, {$push:{valoration:newVal} },{new:true}).populate('userComments.idUser')
+        .then(respuesta =>{
+            return res.json({success:true, response:respuesta})
+        })
+        .catch(error=>{
+            return res.json({success:false, response:error})
+        })   }
+    
 
     }
 
