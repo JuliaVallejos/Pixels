4ef63f94495b07d6d830e082f4af3b38ddc12650
Julia Vallejos <jval.benes@gmail.com>
2021-02-21 01:16:05 -0300

    console

--- frontend/src/components/Games.js
+++ frontend/src/components/Games.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom'
 
  
   const Games = (props) =>{
+      console.log(props)
      
     const {newGamesList,filterGames} = props
     const [newOrder,setNewOrder] =  useState([])
