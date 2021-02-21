a03e8b28f30e321ba79e312c7d6296a0c2818252
Jordi Belmonte <jorte.03@gmail.com>
2021-02-21 03:00:09 -0300

    Final Styles

--- frontend/src/components/HomeNews.js
+++ frontend/src/components/HomeNews.js
@@ -4,7 +4,7 @@ const HomeNews = ({news:{newsImg,newsTitle,news,newsDescription}}) =>{
         <>
             <div className="bordes estiloCardIt estiloCard cardHijo justifyFlexEnd homeNewsCards" 
                 style={{ backgroundImage: `url("/newsImages/${newsImg}")` }}>
-                <div className="news ">
+                <div className="news">
                     <div className="texto">
                         <h3 className="tituloNoticia">{newsTitle}</h3>
                         <h2 className="descripcionNoticia">{newsDescription}</h2>
