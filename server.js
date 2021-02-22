5bcd558d0a2c4f97d6f58dd89da8cd67086beee6
Fran <franciscoaflorentin@hotmail.com>
2021-02-22 01:21:48 -0300

    asdasd

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const cors= require("cors");
 require("dotenv").config();
 // require('./db');
 const express=require("express");
+const path=require("path");
 const router=require("./routes/index");
 const fileUpload =require("express-fileupload");
 require("./config/database");
@@ -14,6 +15,15 @@ app.use(cors());
 app.use(fileUpload());
 
 app.use("/api",router);
+
+if(process.env.NODE_ENV==="production"){
+    console.log("adasdsadsad")
+    app.use(express.static("client/build"))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname+"/cient/build/index.html"))
+    })
+}
+
 const port=process.env.PORT;
 
 const host=process.env.HOST || '0.0.0.0';
