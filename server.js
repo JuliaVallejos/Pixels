4aafab7e38d3a1d008dfa550f8af376c43b65257
Fran <franciscoaflorentin@hotmail.com>
2021-02-22 00:32:04 -0300

    asdasd

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(cors());
 app.use(fileUpload());
 
 app.use("/api",router);
-
 const port=process.env.PORT;
+
 const host=process.env.HOST || '0.0.0.0';
 
 app.listen(port,host,()=>console.log("App listening on port 4000"))
