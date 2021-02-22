5077dca2300a7caa475ea82a7194d62814a96b22
Fran <franciscoaflorentin@hotmail.com>
2021-02-21 23:59:48 -0300

    asds

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ app.use("/api",router);
 const port=process.env.PORT;
 const host=process.env.HOST || '0.0.0.0';
 
-app.listen(port,()=>console.log("App listening on port 4000"))
+app.listen(port,host,()=>console.log("App listening on port 4000"))
